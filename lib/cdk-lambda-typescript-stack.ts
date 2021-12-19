09e8d498b167b0ba84be2a6507fbae39088d9b50
tokku5552 <physics.0121@gmail.com>
2021-12-19 23:31:32 +0900

    不要な記載を削除

--- lib/cdk-lambda-typescript-stack.ts
+++ lib/cdk-lambda-typescript-stack.ts
@@ -1,30 +1,18 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Function, Runtime, AssetCode } from 'aws-cdk-lib/aws-lambda';
-import { aws_s3 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class CdkLambdaTypescriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucket = new aws_s3.Bucket(this, 'FunctionStore');
-
-    const lambdaPolicy = new PolicyStatement();
-    lambdaPolicy.addActions("s3:ListBucket");
-    lambdaPolicy.addResources(bucket.bucketArn);
-
-    const lambdaFunction = new Function(this, 'HelloWorld', {
+    new Function(this, 'HelloWorld', {
       functionName: 'HelloWorld',
       handler: "handler.handler",
       runtime: Runtime.NODEJS_14_X,
       code: new AssetCode(`./src`),
       memorySize: 512,
       timeout: Duration.seconds(10),
-      environment: {
-        bucket: bucket.bucketName,
-      },
-      initialPolicy: [lambdaPolicy],
     });
   }
 }
